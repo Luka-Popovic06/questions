0e821653b473113334ce47588c82dc4ed0b3691b
Luka Popovic <lukapopovic0111@gmail.com>
2025-10-02 23:19:29 +0200

    Add base styles for layout, list, and questions (body, title, list, list-element, btn, icon, question, answer)

--- src/assets/info.js
+++ src/assets/info.js
@@ -1,26 +1,31 @@
-export const questions = [
+export const questionsArray = [
   {
-    question: "Do I have to allow the use of cookies?",
+    id: 1,
+    question: "Do I Have To Allow The Use Of Cookies?",
     answer:
       "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
   },
   {
-    question: "How do I change my My Page password?",
+    id: 2,
+    question: "How Do I Change My My Page Password?",
     answer:
       "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
   },
   {
-    question: "What is BankID?",
+    id: 3,
+    question: "What Is BankID?",
     answer:
       "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
   },
   {
-    question: "Whose birth number can I use?",
+    id: 4,
+    question: "Whose Birth Number Can I Use?",
     answer:
       "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
   },
   {
-    question: "When do I recieve a password ordered by letter?",
+    id: 5,
+    question: "When Do I Recieve a Password Ordered By Letter?",
     answer:
       "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan",
   },
